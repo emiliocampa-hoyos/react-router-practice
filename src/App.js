d571d79b024e8f6b9db370b0d86ef9f229d2e944
User <campa813@gmail.com>
2019-03-23 13:40:53 -0600

    Client side routers without refresh using LINK component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+
+let Tables = () => <div>Tables</div>;
+let Charts = () => <div>Charts</div>;
+let Things = () => <div>Things</div>;
 
 function App() {
   return (
+    <Router>
     <div> 
       <div id="wrapper">
         <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -11,11 +18,14 @@ function App() {
           <SideNav />
         </nav>
         <div style={{backgroundColor: "white"}}>
-          {/* PUT YOUR ROUTES HERE */}
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/charts" component={Charts} />
+          <Route path="/tables" component={Tables} />
+          <Route path="/t" component={Things} />
         </div>
       </div>
     </div>
-
+    </Router>
   );
 }
 
